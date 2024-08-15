146e882e4dc442247ef4505f5a6a1ff9bdaa7202
hyunho4532 <kyounguk192@gmail.com>
2024-08-15 15:15:36 +0900

    Dev . supabase setting && data insert test

--- src/component/dialog/MeetingRoom.tsx
+++ src/component/dialog/MeetingRoom.tsx
@@ -1,27 +1,41 @@
 import { MeetingRoom } from "@mui/icons-material"
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
+import { supabase } from "../../supabase";
+
+export type MeetingRoomProps = {
+    open: boolean, 
+    sidebarData: {
+        title: string;
+        icon: JSX.Element;
+        link: string;
+    }[], 
+    setSidebarData: React.Dispatch<React.SetStateAction<{
+        title: string;
+        icon: JSX.Element;
+        link: string;
+    }[]>>
+}
 
 export function MeetingRoomDialog({
     open, 
     sidebarData, 
     setSidebarData
-}: {
-    open: any, 
-    sidebarData: any, 
-    setSidebarData: any
-}) {
+}: MeetingRoomProps) {
 
     const [meetingRoom, setMeetingRoom] = useState('');
 
-    const addMeetingRoom = () => {
-        setSidebarData([...sidebarData, {
-            title: meetingRoom,
-            icon: <MeetingRoom />,
-            link: ''
-        }])
+    const addMeetingRoom = async () => {
+
+        console.log(meetingRoom);
+
+        const { data, error } = await supabase.from('MeetingRoom').insert({ title: meetingRoom });
+    
+        if (error) {
+            console.log("에러 발생" + error);
+        }
     }
 
-    const onChange = (event: any) => {
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
         setMeetingRoom(event.target.value);
     }
 
@@ -30,15 +44,20 @@ export function MeetingRoomDialog({
             <h2 className="DialogTitle">
                 회의방 추가하기
             </h2>
-            <div className="DialogContent">
-                <h3>용도</h3>
+            <div className="DialogPurpose">
+                <h3>용도 선택: </h3>
                 <select className="DialogSelect">
                     <option>1</option>
                 </select>
             </div>
-            <input className="DialogInput" 
+
+            <div className="DialogMeeting">
+                <h3>이름 입력: </h3>
+                <input className="DialogInput" 
                    placeholder="회의방 이름을 입력해주세요!"
                    onChange={onChange}  />
+            </div>
+
             <button className="DialogButton" 
                     onClick={() => addMeetingRoom()}>희의방 추가 완료</button>
         </dialog>
